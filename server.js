1579b1d8158e8278168a4a183ca14dcecc051251
Azula100 <zulchimegzula@gmail.com>
2026-06-10 09:37:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
 
-dotenv.config({ path:'./config/config.env'})
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: './config/config.env' })
+}
 connectDB()
 
 const app = express()
