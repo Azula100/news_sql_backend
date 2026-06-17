72f1ffd3a3562e7ad904a339950d098485623f51
Azula100 <zulchimegzula@gmail.com>
2026-06-17 12:58:59 +0800

    Update newsRoute.js

--- routes/newsRoute.js
+++ routes/newsRoute.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const router = express.Router()
-const path = require('path')
-const {
-    getAllNews, getNewsById, createNews, updateNews, deleteNews, uploadNewsPhoto
-} = require('../controller/newsController')
+const router  = express.Router()
+const { getAllNews, getNewsById, createNews, updateNews, deleteNews } = require('../controller/newsController')
 const { protect, authorize } = require('../middleware/auth')
 
 router.get('/',    getAllNews)
@@ -11,4 +8,5 @@ router.get('/:id', getNewsById)
 router.post('/',   protect, authorize('user', 'editor', 'admin'), createNews)
 router.put('/:id', protect, updateNews)
 router.delete('/:id', protect, deleteNews)
+
 module.exports = router
