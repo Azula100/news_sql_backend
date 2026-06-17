2831835de7970030a89a232cd86d6b40f54f1824
Azula100 <zulchimegzula@gmail.com>
2026-06-17 12:59:28 +0800

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,18 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const { login, register, getUsers, getUser,
-        updateUser, deleteUser, logout } = require('../controller/userController');
-const { protect, authorize } = require('../middleware/auth'); 
+const express = require('express')
+const router  = express.Router()
+const { register, login, logout, getUsers, getUser, updateUser, deleteUser } = require('../controller/userController')
+const { protect } = require('../middleware/auth')
 
-router.route('/login').post(login);       
-router.route('/logout').post(logout);     
-router.route('/register').post(register); 
+router.post('/login', login)
+router.get('/logout', logout)
+router.route('/').post(register).get(protect, getUsers)
+router.route('/:id').get(protect, getUser).put(protect, updateUser).delete(protect, deleteUser)
 
-router.route('/').get(protect, authorize('admin'), getUsers);
-
-router.route('/:id')
-    .get(protect, getUser)
-    .put(protect, authorize('admin'), updateUser)
-    .delete(protect, authorize('admin'), deleteUser)
-
-module.exports = router;
+module.exports = router
